3e9a54c911fde5d8f7f3416db953d117ec893939
stelios <stelios05@gmail.com>
2023-11-04 17:10:30 +0000

    hover sets color

--- js-file.js
+++ js-file.js
@@ -1,15 +1,24 @@
+function paintCell(event){
+ 
+  const div=event.target
+  div.style.backgroundColor='blue';
+}
 function init(){
   const container = document.querySelector('#container');
   const size=16;
   for(r=0;r<size;r++){
-    const row = document.createElement('div');
+    const row = document.createElement('div');    
+    row.classList.add("row")
     for(c=0;c<size;c++){
       const cell=document.createElement('div');
-      cell.textContent=`${r}_${c}`;
+      //cell.textContent=`${r}_${c}`;
+      cell.classList.add("cell");
+      cell.onmouseover=(paintCell);
       row.appendChild(cell);
     }
     container.appendChild(row)
   }
 }
+
 
 init();
