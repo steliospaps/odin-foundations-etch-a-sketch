2cf425a42a47e0b95db8a642b668f0659ef538d3
stelios <stelios05@gmail.com>
2023-11-04 18:15:26 +0000

    button to set size of grid

--- js-file.js
+++ js-file.js
@@ -3,9 +3,11 @@ function paintCell(event){
   const div=event.target
   div.style.backgroundColor='blue';
 }
-function init(){
+function init(size){
   const container = document.querySelector('#container');
-  const size=16;
+  while(container.firstChild){
+    container.removeChild(container.lastChild);
+  }
   for(r=0;r<size;r++){
     const row = document.createElement('div');    
     row.classList.add("row")
@@ -18,7 +20,19 @@ function init(){
     }
     container.appendChild(row)
   }
+
 }
 
 
-init();
+init(32);
+
+document.querySelector('#btSetSize').addEventListener('click',e=>{
+  const size=prompt("Set GridSize (10-100)");
+  if(size<10){
+    init(10)
+  }else if(size>100){
+    init(100)
+  }else{
+    init(size)
+  }
+})
